78440de9c760f60a5f5cd86b4f1a7e037108cfe9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-19 13:11:00 +0300

    Create 05-Constants.sol

---
+++ 05-Constants.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  0.8.17;
+
+contract ConstantExample {
+
+    uint public constant INITIAL_BUDGET_USD = 0;
+
+    string public constant FOUNDER = "MUSTAFA BUYUKDERELI";
+
+    uint32 public constant START_YEAR = 2024;
+
+}
