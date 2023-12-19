127a29b7df5ae3a1c684b45e4cc4822d5afba5bd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-19 13:18:57 +0300

    Update 06-Immutable.sol

--- 06-Immutable.sol
+++ 06-Immutable.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+contract Immutables {
+
+    address public immutable COMPANY_ADDRESS;
+
+    uint public immutable ANNUAL_TARGET;
+
+    constructor(uint _target) {
+
+        COMPANY_ADDRESS = msg.sender;
+
+        ANNUAL_TARGET = _target;
+    }
+
+}
