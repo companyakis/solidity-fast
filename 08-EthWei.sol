09a3b4b2c786cbfd0ba1bb5cb8ed391d9e08cd90
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-20 16:32:50 +0300

    Create 08-EthWei.sol

---
+++ 08-EthWei.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract EthWeiUsage {
+
+    //1 eth is equal to 10^18 wei
+
+    function showResult() public pure  returns (bool) {
+        bool isItTrue;
+        isItTrue = 1 ether == 1000000000000000000 wei;
+        return isItTrue; //true
+    }
+}
