08c06ffb580a46d9f35c942bb851eca2b4c7080f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-20 16:44:12 +0300

    Update 08-EthWei.sol

--- 08-EthWei.sol
+++ 08-EthWei.sol
@@ -7,7 +7,9 @@ contract EthWeiUsage {
 
     function showResult() public pure  returns (bool) {
         bool isItTrue;
-        isItTrue = 1 ether == 1000000000000000000 wei;
+        //isItTrue = 1 ether == 1000000000000000000 wei;
+        isItTrue = 1 ether == 1e18 wei;
+
         return isItTrue; //true
     }
 }
