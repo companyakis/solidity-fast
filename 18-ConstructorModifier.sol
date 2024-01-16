6d717ed7667a1d569145dac110cc1caea5b65290
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-16 12:43:20 +0300

    Update 18-ConstructorModifier.sol

--- 18-ConstructorModifier.sol
+++ 18-ConstructorModifier.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract ConstructorModifier {
+
+    //part 1 constructor
+
+    //contract owner and his/her message
+    address public owner;
+    string public ownerMessage;
+
+    constructor() {
+
+        owner = msg.sender;
+        ownerMessage = "Mustafa Buyukdereli says Solidity is a good language.";
+
+    }
+
+    //part 2 modifier
+
+    modifier onlyContractOwner() {
+        require(msg.sender == owner, "The owner can be changed by Mustafa Buyukdereli:(");
+        _;
+    }
+
+    function changeOwner (address _newOwner) external onlyContractOwner {
+        owner = _newOwner;
+    }
+}
