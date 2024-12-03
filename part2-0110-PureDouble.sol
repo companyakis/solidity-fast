d34a68b74a1cf52d38e9f4e00d26b5e8a1b917d1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 21:43:18 +0300

    Create part2-0110-PureDouble.sol

---
+++ part2-0110-PureDouble.sol
@@ -0,0 +1,14 @@
+/*Your Goal: Double Uint
+Create an external, pure function called double which takes a uint parameter and doubles it. It should return this doubled uint value.*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    function double(uint num) external pure returns (uint) {
+
+        return num * 2;
+    }
+
+}
