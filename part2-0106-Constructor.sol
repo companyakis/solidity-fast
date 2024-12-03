9fed91af86dcbf64d9fd90f8e3db32a273a9c86c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 21:30:15 +0300

    Create part2-0106-Constructor.sol

---
+++ part2-0106-Constructor.sol
@@ -0,0 +1,4 @@
+//Your Goal: Overload Double
+//Create another pure external function double which takes two uint parameters.
+//Double both of the arguments and return both of them in the same order they were passed into the function.
+
