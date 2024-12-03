037df8fb98331526e90ae60996575716e1ea8f08
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 21:30:53 +0300

    Update part2-0106-Constructor.sol

--- part2-0106-Constructor.sol
+++ part2-0106-Constructor.sol
@@ -1,4 +1,17 @@
-//Your Goal: Overload Double
-//Create another pure external function double which takes two uint parameters.
-//Double both of the arguments and return both of them in the same order they were passed into the function.
+/*Your Goal: Unsigned Int Constructor
+Create a constructor which will take a uint as an argument.
+Store this uint value inside a public storage variable called x.*/
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    uint public x;
+
+    constructor(uint _x) {
+
+        x = _x;
+    }
+
+}
